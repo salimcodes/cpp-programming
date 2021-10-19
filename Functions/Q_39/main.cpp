b270b8d9d0745738b61f98293a2291cb681d11ca
Salim Ọlánrewájú Oyinlọlá <salimoyinlola@gmail.com>
2021-10-19 06:21:07 +0100

    Update main.cpp

--- Functions/Q_39/main.cpp
+++ Functions/Q_39/main.cpp
@@ -1,25 +1,7 @@
 #include <iostream>
 
 using namespace std;
-/*
-The C++ Water and Ice Company has another problem for us to solve.When
-water freezes into ice it expands in volume but still weighs the same.The same
-volume of liquid water expands to fill a volume approximately ten percent
-larger. (If you had 100 ounces of liquid water and froze it, the corresponding
-ice volume is equivalent to 110 fluid ounces.)
-The company produces cubes of ice that it sells in bags. To make the ice,
-square trays have square compartments that are filled with water. For example,
-trays may contain 100 * 100 cubes,each cube is intended to be one square
-inch.The ice trays are filled with water and frozen.This program allows the ice
-makers to enter ice cube tray sizes and cube size. We determine how much
-water (in gallons) is required to fill that tray so that the resultant cubes have
-expanded to completely fill the cube but doesn’t expand over the lip of the
-tray! The program functions are AskForTraySize (asks for the number of
-cubes on the tray), AskForCubeSize (asks for single dimension for the cubes
-side in inches) and CalcGallons that is given the tray and cube sizes. The
-function returns the gallons needed. Remember to use the water to ice expansion
-information to obtain accurate results!
-*/
+
 
 float AskForTraySize();
 float AskForCubeSize();
