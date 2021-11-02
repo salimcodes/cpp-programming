912e1a1f362e9436ee86ad58ed186b73a7101d0a
Salim Ọlánrewájú Oyinlọlá <salimoyinlola@gmail.com>
2021-11-02 03:29:46 +0100

    Comments Added

--- Functions-2/Q_25/main.cpp
+++ Functions-2/Q_25/main.cpp
@@ -3,16 +3,6 @@
 
 using namespace std;
 
-/*
-Write a program that calls a function AskRandHCyl.This function asks for the
-radius and height of a cylinder. From main, pass these dimensions to the
-CalcVolAndSA function, which uses pointers or references to obtain the surface
-area and volume of the cylinder. Print the dimensions and calculated values
-from main showing four decimal places of accuracy. Refer to Chapter 2,
-Figure 2-2 and Figure 2-14.The volume of a cylinder is:
-where pi is 3.14159265, r is the radius, and h is the height.
-*/
-
 void AskRandHCyl(double *, double *);  //Using pointers. Hence, call by reference
 double CalcVolAndSA(double, double);
 int main()
